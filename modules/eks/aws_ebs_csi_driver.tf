8db40a576ad82c90ab586abee55e262e04619d38
yanashapka <yana.shapka.test@gmail.com>
2025-07-10 22:46:14 +0300

    updated policy

--- modules/eks/aws_ebs_csi_driver.tf
+++ modules/eks/aws_ebs_csi_driver.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role" "ebs_csi_driver_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/Amazon_EBS_CSI_DriverPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi_driver_role.name
 }
 
